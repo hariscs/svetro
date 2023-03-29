c70739768750a4c3860a6cd5f37efb4f9f0bb6f2
Haris Shah <harisshah385@gmail.com>
2023-03-29 19:59:42 +0500

    reset router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<BrowserRouter>
 			<ScrollToTop />
 			<App />
 		</BrowserRouter>
